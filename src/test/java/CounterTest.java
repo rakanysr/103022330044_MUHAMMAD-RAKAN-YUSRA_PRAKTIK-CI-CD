829e64336e31d598d9622c66071fb5a1dbf7edf7
rakanysr <mrakan711@gmail.com>
2026-06-11 08:04:24 +0700

    Update CounterTest.java

--- src/test/java/CounterTest.java
+++ src/test/java/CounterTest.java
@@ -16,7 +16,7 @@ public class CounterTest {
 		
 		testCounter.reset();
 		
-		assertEquals(testCounter.getCount(), 0);
+		assertEquals(testCounter.getCount(), 1);
 	}
 
 	@Test
@@ -25,7 +25,7 @@ public class CounterTest {
 		
 		for(int i = 1; i <10; i++) {
 			testCounter.increment();
-			assertEquals(testCounter.getCount(), i);
+			assertEquals(testCounter.getCount(), 1);
 		}
 	}
 
@@ -35,7 +35,7 @@ public class CounterTest {
 		
 		for(int i = 1; i <10; i++) {
 			testCounter.decrement();
-			assertEquals(testCounter.getCount(), i * -1);
+			assertEquals(testCounter.getCount(), 1);
 		}
 	}
 
